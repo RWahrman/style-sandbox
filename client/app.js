2a6a990bbc115fbeb60bd8a7341e717cc5f659b7
RWahrman <rebecca.wahrman@gmail.com>
2020-05-01 16:28:48 -0400

    style of boxes

--- client/app.js
+++ client/app.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
-import {Navbar} from './components'
-import Routes from './routes'
+// import {Navbar} from './components'
+// import Routes from './routes'
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      <Routes />
+    <div id="site-div">
+      <div id="site-header">
+        <h1>Becky's Style Sandbox</h1>
+      </div>
+      <div id="sandboxes-container">
+        <div id="sandbox">Sandbox</div>
+        <div id="font-selector">Font Selector</div>
+      </div>
     </div>
   )
 }
