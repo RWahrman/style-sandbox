64266b905fb0b701bca65fcbc7176f518602b109
RWahrman <rebecca.wahrman@gmail.com>
2020-05-01 15:26:16 -0400

    feat: tryig out background styles

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -6,7 +6,7 @@ import {logout} from '../store'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
-    <h1>BOILERMAKER</h1>
+    <h1>Becky's Style Sandbox</h1>
     <nav>
       {isLoggedIn ? (
         <div>
